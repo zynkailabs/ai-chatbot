efc4d8ca7586bca90ad68b3bde5cbe84e8e64633
Amanpreet Singh <amanpreetsingh3095@gmail.com>
2024-04-10 18:29:15 +0000

    Immigration-ify the empty screen

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -6,16 +6,16 @@ import { IconArrowRight } from '@/components/ui/icons'
 
 const exampleMessages = [
   {
-    heading: 'Explain technical concepts',
-    message: `What is a "serverless function"?`
+    heading: 'The green card process',
+    message: `How does the green card process work?`
   },
   {
-    heading: 'Summarize an article',
-    message: 'Summarize the following article for a 2nd grader: \n'
+    heading: 'Traveling with a pending H1B application',
+    message: 'Can I travel internationally while on an H1B visa?`
   },
   {
-    heading: 'Draft an email',
-    message: `Draft an email to my boss about the following: \n`
+    heading: 'Switching employers on an H1B visa',
+    message: `Can I switch employers while on an H1B visa?`
   }
 ]
 
@@ -24,15 +24,15 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
         <h1 className="mb-2 text-lg font-semibold">
-          Welcome to the Supabaseified Next.js AI Chatbot!
+          Welcome to Immigration AI!
         </h1>
-        <p className="mb-2 leading-normal text-muted-foreground">
+        {/* <p className="mb-2 leading-normal text-muted-foreground">
           This is an open source AI chatbot app template built with{' '}
           <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
           <ExternalLink href="https://supabase.com">Supabase</ExternalLink>.
-        </p>
+        </p> */}
         <p className="leading-normal text-muted-foreground">
-          You can start a conversation here or try the following examples:
+          You can start a conversation below or try some examples:
         </p>
         <div className="mt-4 flex flex-col items-start space-y-2">
           {exampleMessages.map((message, index) => (
