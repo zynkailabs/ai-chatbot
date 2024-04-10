b82ebed767c8ad056d97b69681dffdd007322209
Amanpreet Singh <amanpreetsingh3095@gmail.com>
2024-04-10 18:14:03 +0000

    Remove the next logo

--- components/header.tsx
+++ components/header.tsx
@@ -25,7 +25,7 @@ export async function Header() {
   return (
     <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between border-b bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl">
       <div className="flex items-center">
-        {session?.user ? (
+        {session?.user && (
           <Sidebar>
             <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
               {/* @ts-ignore */}
@@ -36,11 +36,6 @@ export async function Header() {
               <ClearHistory clearChats={clearChats} />
             </SidebarFooter>
           </Sidebar>
-        ) : (
-          <Link href="/" target="_blank" rel="nofollow">
-            <IconNextChat className="mr-2 h-6 w-6 dark:hidden" inverted />
-            <IconNextChat className="mr-2 hidden h-6 w-6 dark:block" />
-          </Link>
         )}
         <div className="flex items-center">
           <IconSeparator className="h-6 w-6 text-muted-foreground/50" />
