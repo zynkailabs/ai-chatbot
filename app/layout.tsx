197c78a7de515789ad59802a53c1117180efb7e0
Amanpreet Singh <amanpreetsingh3095@gmail.com>
2024-06-13 02:02:52 +0000

    Remove header

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <Providers attribute="class" defaultTheme="system" enableSystem>
           <div className="flex min-h-screen flex-col">
             {/* @ts-ignore */}
-            <Header />
+            {/* <Header /> */}
             <main className="flex flex-1 flex-col bg-muted/50">{children}</main>
           </div>
           <TailwindIndicator />
