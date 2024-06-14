78356485641ee1f9ac9d8272c43a5155ec9cbc49
Amanpreet Singh <amanpreetsingh3095@gmail.com>
2024-06-14 20:13:29 +0000

    remove interrupt completely

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -16,8 +16,8 @@ export interface ChatPanelProps
     | 'input'
     | 'setInput'
   > {
-  id?: string,
-  isLoading: boolean,
+  id?: string
+  isLoading: boolean
 }
 
 export function ChatPanel({
@@ -33,7 +33,7 @@ export function ChatPanel({
     <div className="fixed inset-x-0 bottom-0 bg-gradient-to-b from-muted/10 from-10% to-muted/30 to-50%">
       <ButtonScrollToBottom />
       <div className="mx-auto sm:max-w-2xl sm:px-4">
-        <div className="flex h-10 items-center justify-center">
+        {/* <div className="flex h-10 items-center justify-center">
           {isLoading && (
             <Button
               variant="outline"
@@ -41,10 +41,10 @@ export function ChatPanel({
               className="bg-background"
             >
               <IconStop className="mr-2" />
-              Stop generating
+              Interrupt
             </Button>
           )}
-        </div>
+        </div> */}
         <div className="space-y-4 border-t bg-background px-4 py-2 shadow-lg sm:rounded-t-xl sm:border md:py-4">
           <PromptForm
             onSubmit={async value => {
