fbc873039a74c951a0c30083451e64baa761e1c4
Amanpreet Singh <amanpreetsingh3095@gmail.com>
2024-06-27 23:36:32 +0000

    goddamn dark mode

--- components/empty-header.tsx
+++ components/empty-header.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 export async function EmptyHeader() {
   return (
-    <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between border-b px-4 backdrop-blur-xl bg-background dark:bg-background"
+    <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between backdrop-blur-xl bg-background dark:bg-foreground"
       style={{ filter: 'invert(1)' }}>
     </header>
   )
