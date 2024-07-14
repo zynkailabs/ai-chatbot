07ee4ce3c557827aa6a8008ec2b85dbc2d4c7802
Amanpreet Singh <amanpreetsingh3095@gmail.com>
2024-07-14 14:18:01 +0000

    update header background color to corpo color

--- components/text-header.tsx
+++ components/text-header.tsx
@@ -3,8 +3,7 @@ import * as React from 'react'
 export async function TextHeader() {
   return (
     <header
-      className="sticky top-0 z-50 flex h-16 w-full items-center justify-center backdrop-blur-xl"
-      style={{ backgroundColor: '#bf3839' }}
+      className="sticky top-0 z-50 flex h-16 w-full items-center justify-center backdrop-blur-xl bg-corpoHeaderBackground"
     >
       <h1 style={{ color: 'white', fontSize: '1.125rem', fontWeight: 'bold' }}>
         CS Campus Genie
