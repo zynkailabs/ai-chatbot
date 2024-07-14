b4f9907aa019cfdf5ca5f49701db26bf0c69d4c5
Amanpreet Singh <amanpreetsingh3095@gmail.com>
2024-07-14 16:22:48 +0000

    add back code block

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -36,6 +36,36 @@ export function ChatMessage({ message, ...props }: ChatMessageProps) {
             p({ children }) {
               return <p className="mb-2 last:mb-0 text-background text-inherit">{children}</p>
             },
+            code({ node, inline, className, children, ...props }) {
+              if (children.length) {
+                if (children[0] == '▍') {
+                  return (
+                    <span className="mt-1 animate-pulse cursor-default">▍</span>
+                  )
+                }
+
+                children[0] = (children[0] as string).replace('`▍`', '▍')
+              }
+
+              const match = /language-(\w+)/.exec(className || '')
+
+              if (inline) {
+                return (
+                  <code className={className} {...props}>
+                    {children}
+                  </code>
+                )
+              }
+
+              return (
+                <CodeBlock
+                  key={Math.random()}
+                  language={(match && match[1]) || ''}
+                  value={String(children).replace(/\n$/, '')}
+                  {...props}
+                />
+              )
+            }
           }}
         >
           {message.content}
