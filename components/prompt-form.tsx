f09c24e9ac44a551f68c287b9564be3c014877f0
Ritiz Tambi <ritiztambi@gmail.com>
2024-07-18 11:22:23 +0000

    removing conflict for bg-inherit bg-background

--- components/prompt-form.tsx
+++ components/prompt-form.tsx
@@ -46,7 +46,7 @@ export function PromptForm({
       }}
       ref={formRef}
     >
-      <div className="relative flex max-h-60 w-full grow flex-col overflow-hidden bg-inherit px-8 sm:rounded-md sm:border sm:px-12 bg-background">
+      <div className="relative flex max-h-60 w-full grow flex-col overflow-hidden bg-inherit px-8 sm:rounded-md sm:border sm:px-12">
         <Tooltip>
           <TooltipTrigger asChild>
             <Link
