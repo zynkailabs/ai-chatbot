434d1151ab72c4832c0f5011108644960ee4d89f
Ritiz Tambi <ritiztambi@gmail.com>
2024-07-18 08:30:39 -0400

    bg color changes for header/footer and footer ssample text change (#7)
    * bg color changes for header/footer and footer ssample text change
    
    * header text replaced by provided header asset
    
    * removing font_type from text-header
    
    * removing conflict for bg-inherit bg-background

--- components/text-header.tsx
+++ components/text-header.tsx
@@ -2,10 +2,14 @@ import * as React from 'react'
 
 export async function TextHeader() {
   return (
-    <header
-      className="sticky top-0 z-50 flex h-16 w-full items-center justify-center backdrop-blur-xl bg-corpoHeaderBackground"
-    >
-      <h1 style={{ color: 'white', fontSize: '1.125rem', fontWeight: 'bold' }}>
+    <header className="sticky top-0 z-50 flex h-16 w-full items-center justify-center backdrop-blur-xl bg-corpoHeaderBackground">
+      <h1
+        style={{
+          color: 'white',
+          fontSize: '1.125rem',
+          fontWeight: 'bold'
+        }}
+      >
         CS Campus Genie
       </h1>
     </header>
