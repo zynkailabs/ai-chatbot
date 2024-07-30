209e43a71a9be757d4b2986c9feb52c207c4e2e2
Ritiz Tambi <ritiztambi@gmail.com>
2024-07-30 19:05:53 +0000

    New dummy data and fns for corpo requirements

--- app/api/dummy_functions.ts
+++ app/api/dummy_functions.ts
@@ -12,6 +12,9 @@ interface DummyData {
     University_Policy: string
     Subject_Prerequisites: string
     Specialization_Requirements: string
+    University_Schedule: string
+    Class_Schedule: string
+    Housing_Availability: string
 }
 
 class DummyFunctionsClient {
@@ -42,10 +45,10 @@ class DummyFunctionsClient {
     }
 
     getRandomArbitrary(min: number, max: number) {
-        return Math.random() * (max - min) + min
+        return Math.floor(Math.random() * (max - min)) + min
     }
 
-    applyLeave(start_date: string, end_date: string) {
+    createLeaveApplication(start_date: string, end_date: string) {
         let start = new Date(start_date)
         let end = new Date(end_date)
         let diff = Math.abs(end.getTime() - start.getTime())
@@ -63,16 +66,34 @@ class DummyFunctionsClient {
         return `Your leave application for ${days} days starting from ${start_date} to ${end_date} has been submitted successfully. Please use the issue ticket number A${application_number} for future reference and tracking.`
     }
 
+    createHousingIssueApplication(issue: string) {
+        let application_number = this.getRandomArbitrary(1000000, 9999999)
+        console.log(application_number)
+        if (application_number % 2 == 0) {
+            //generated number is even
+            if (application_number == 9999999) {
+                application_number = application_number - 1
+            } else {
+                application_number = application_number + 1
+            }
+        }
+        return `Your application for ${issue} has been submitted successfully. Please use the issue ticket number H${application_number} for future reference and tracking. For more details, please contact Residential Services Center for further assistance regarding your application request.`
+    }
+
     trackTicketStatus(ticket_number: string) {
         let num = Number(ticket_number.slice(1))
-        if (ticket_number.startsWith('A')) {
+        if (ticket_number.startsWith('A') || ticket_number.startsWith('H')) {
+            let request_type = 'Leave Application'
+            if (ticket_number.startsWith('H')) {
+                request_type = 'Kitchen Sink repair'
+            }
             if (num % 2 == 0) {
-                return `Ticket ${ticket_number} has been approved.`
+                return `Request for ${request_type} with ticket number ${ticket_number} has been approved.`
             } else {
-                return `Ticket ${ticket_number} is currently in progress.`
+                return `Rquest for ${request_type} with ticket number ${ticket_number} is in progress.`
             }
         } else {
-            return `Invalid ticket. Please provide a valid ticket number.`
+            return `Invalid ticket. Please provide a valid ticket number. Housing related tickets start with 'H' and all other administrative tickets start with 'A'`
         }
     }
 
@@ -86,6 +107,15 @@ class DummyFunctionsClient {
 
     getSpecializationRequirements() {
         return this.dummy_data['Specialization_Requirements']
+    }
+    getUniversitySchedule() {
+        return this.dummy_data['University_Schedule']
+    }
+    getClassSchedule() {
+        return this.dummy_data['Class_Schedule']
+    }
+    getHousingAvailability() {
+        return this.dummy_data['Housing_Availability']
     }
 }
 export default DummyFunctionsClient
