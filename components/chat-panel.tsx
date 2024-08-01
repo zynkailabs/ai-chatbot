35726e2b325e53151f76d0e49096deb0e88cb635
Amanpreet Singh <amanpreetsingh3095@gmail.com>
2024-08-01 20:48:49 +0000

    user input area

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -49,7 +49,7 @@ export function ChatPanel({
             </Button>
           )}
         </div> */}
-        <div className="space-y-4 border-t bg-corpoChatPanelBackground px-4 py-2 shadow-lg sm:rounded-t-xl sm:border md:py-4">
+        <div className="space-y-4 border-t bg-userInputArea px-4 py-2 shadow-lg sm:rounded-t-xl sm:border md:py-4">
           <PromptForm
             onSubmit={async value => {
               await append(
