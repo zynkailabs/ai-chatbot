1e7f514354982d496f19debac3901f9b592ed30f
Amanpreet Singh <amanpreetsingh3095@gmail.com>
2024-08-02 15:00:24 +0000

    remove ugly corpo header

--- components/empty-header.tsx
+++ components/empty-header.tsx
@@ -2,8 +2,7 @@ import * as React from 'react'
 
 export async function EmptyHeader() {
   return (
-    <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between backdrop-blur-xl bg-background dark:bg-foreground"
-      style={{ filter: 'invert(1)' }}>
+    <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between backdrop-blur-xl bg-headerBackground dark:bg-headerBackground">
     </header>
   )
 }
