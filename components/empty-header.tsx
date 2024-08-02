6c5aa8851b2a8f47e78ae4c3d62d139cd9e1b0f0
Amanpreet Singh <amanpreetsingh3095@gmail.com>
2024-08-02 15:51:19 +0000

    empty header does not need to be async

--- components/empty-header.tsx
+++ components/empty-header.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react'
 
-export async function EmptyHeader() {
+export function EmptyHeader() {
   return (
-    <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between backdrop-blur-xl bg-headerBackground dark:bg-headerBackground">
-    </header>
+    <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between backdrop-blur-xl bg-headerBackground dark:bg-headerBackground"></header>
   )
 }
