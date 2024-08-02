66147d92787f5c68c4f697941794e77ee2e3f532
Amanpreet Singh <amanpreetsingh3095@gmail.com>
2024-08-02 14:47:15 +0000

    update our own  user text color

--- app/actions.ts
+++ app/actions.ts
@@ -133,7 +133,7 @@ export async function getCustomClientConfig(
           textPrimary: '240, 8%, 12%',
           userChatBubble: '216, 100%, 50%',
           assistantChatBubble: '240, 6%, 97%',
-          userChatBubbleText: '240 5.9% 10%',
+          userChatBubbleText: '0 0% 100%',
           assistantChatBubbleText: '240 5.9% 10%',
           userInputArea: '240, 6%, 97%'
         }
