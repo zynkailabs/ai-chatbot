f4ab64d5c8d874326ca4978bc98f8ebbea46910e
Amanpreet Singh <amanpreetsingh3095@gmail.com>
2024-08-02 16:51:58 +0000

    Host chatbot at a per-client URL

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,6 @@ import { Chat } from '@/components/chat'
 
 export const runtime = 'edge'
 
-const getQueryParamAsString = (param: string | string[] | undefined): string | null =>
-  Array.isArray(param) ? param[0] : param ?? null;
-
-export default function IndexPage({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
-  const id = nanoid();
-
-  const userType = getQueryParamAsString(searchParams['userType']);
-  const userID = getQueryParamAsString(searchParams['userID']);
-
-  return <Chat id={id} additionalData={{ userType, userID }} />;
+export default function IndexPage() {
+  return null;
 }
