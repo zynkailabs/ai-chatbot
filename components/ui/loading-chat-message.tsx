a988ef3b23601ce1180ac9a4405cd6df8f709a8a
Aman Singh <amanpreetsingh3095@gmail.com>
2024-09-15 03:36:48 +0000

    smaller dots

--- components/ui/loading-chat-message.tsx
+++ components/ui/loading-chat-message.tsx
@@ -4,15 +4,15 @@ export function LoadingChatMessage() {
   return (
     <div className="flex items-center justify-center space-x-2">
       <div
-        className="w-2.5 h-2.5 bg-black rounded-full animate-bounce"
+        className="w-1.5 h-1.5 bg-black rounded-full animate-bounce"
         style={{ animationDelay: '0ms' }}
       ></div>
       <div
-        className="w-2.5 h-2.5 bg-black rounded-full animate-bounce"
+        className="w-1.5 h-1.5 bg-black rounded-full animate-bounce"
         style={{ animationDelay: '200ms' }}
       ></div>
       <div
-        className="w-2.5 h-2.5 bg-black rounded-full animate-bounce"
+        className="w-1.5 h-1.5 bg-black rounded-full animate-bounce"
         style={{ animationDelay: '400ms' }}
       ></div>
     </div>
