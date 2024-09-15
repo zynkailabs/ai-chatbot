ddccbbe5d6826078e323c0e2c41bab3a24aeee85
Aman Singh <amanpreetsingh3095@gmail.com>
2024-09-15 03:39:12 +0000

    add margin to top and bottom

--- components/ui/loading-chat-message.tsx
+++ components/ui/loading-chat-message.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export function LoadingChatMessage() {
   return (
-    <div className="flex items-center justify-center space-x-2">
+    <div className="flex items-center justify-center space-x-2 mb-2 mt-2">
       <div
         className="w-1.5 h-1.5 bg-black rounded-full animate-bounce"
         style={{ animationDelay: '0ms' }}
